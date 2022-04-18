a249ab3c306327f71a088596225dd0853bc9ba45
seakr1948 <seakr1948@gmail.com>
2022-04-17 20:57:57 -0700

    Logging path

--- client/src/interceptor/interceptor.js
+++ client/src/interceptor/interceptor.js
@@ -3,11 +3,15 @@ import { useNavigate } from "react-router-dom";
 const axios = require("axios");
 const axiosApiInstance = axios.create();
 
+console.log("interceptor");
 // Request interceptor for API calls
 axiosApiInstance.interceptors.request.use(
   async (config) => {
-    console.log(sessionStorage.getItem("token"))
-    config.headers["authorization"] = sessionStorage.getItem("token");
+    console.log(sessionStorage.getItem("token"));
+
+    config.headers = {
+      authorization: sessionStorage.getItem('token')
+    }
     return config;
   },
   (error) => {
